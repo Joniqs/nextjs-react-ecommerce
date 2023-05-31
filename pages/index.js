230d3d8eb20b3a8c35d47a7e3f8ad130f104937b
Jonatan Kwiatkowski <Joniqs91@gmail.com>
2023-05-31 16:18:56 +0100

    Connected to sanity CMS to fetch data and display relevant information.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ productsData, bannersData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannersData.length && bannersData[0]} />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {productsData?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const productsData = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannersData = await client.fetch(bannerQuery);
+
+  return {
+    props: { productsData, bannersData },
+  };
 };
 
 export default Home;
